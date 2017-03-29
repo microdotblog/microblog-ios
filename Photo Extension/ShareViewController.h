6f16bb7ceb1f04a22b0c8932dc0fbe3f0ab45f6a
Manton Reece <manton@me.com>
2017-03-29 12:23:37 -0500

    Added placeholder for share extension.

---
+++ Photo Extension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Photo Extension
+//
+//  Created by Manton Reece on 3/29/17.
+//  Copyright © 2017 Riverfold Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
