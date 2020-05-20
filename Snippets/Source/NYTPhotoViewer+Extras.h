f717d5d3177366f996f5c2b0847dfcd205df2a0f
Manton Reece <manton@me.com>
2020-05-20 16:09:03 -0500

    Added override to stop hiding status bar in full-screen photos.

---
+++ Snippets/Source/NYTPhotoViewer+Extras.h
@@ -0,0 +1,19 @@
+//
+//  NYTPhotoViewer+Extras.h
+//  Micro.blog
+//
+//  Created by Manton Reece on 5/20/20.
+//  Copyright © 2020 Riverfold Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@import NYTPhotoViewer;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NYTPhotosViewController (Extras)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Snippets/Source/NYTPhotoViewer+Extras.m
@@ -0,0 +1,18 @@
+//
+//  NYTPhotoViewer+Extras.m
+//  Micro.blog
+//
+//  Created by Manton Reece on 5/20/20.
+//  Copyright © 2020 Riverfold Software. All rights reserved.
+//
+
+#import "NYTPhotoViewer+Extras.h"
+
+@implementation NYTPhotosViewController (Extras)
+
+- (BOOL) prefersStatusBarHidden
+{
+	return NO;
+}
+
+@end
