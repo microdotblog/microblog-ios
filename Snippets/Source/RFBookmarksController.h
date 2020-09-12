2d744443ec2247f1eed48a3fbc1e1ecb8263ae97
Manton Reece <manton@me.com>
2020-09-12 14:20:16 -0500

    Added highlights count.

--- Snippets/Source/RFBookmarksController.h
+++ Snippets/Source/RFBookmarksController.h
@@ -12,6 +12,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RFBookmarksController : RFTimelineController
 
+@property (strong, nonatomic) IBOutlet UIButton* highlightsCountButton;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView* progressSpinner;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint* highlightsHeightConstraint;
+
 - (instancetype) initWithEndpoint:(NSString *)endpoint title:(NSString *)title;
 
 @end
--- Snippets/Source/RFBookmarksController.m
+++ Snippets/Source/RFBookmarksController.m
@@ -9,6 +9,9 @@
 #import "RFBookmarksController.h"
 
 #import "RFHighlightsController.h"
+#import "RFClient.h"
+#import "UUHttpSession.h"
+#import "RFMacros.h"
 
 @implementation RFBookmarksController
 
@@ -24,6 +27,44 @@
 - (void) viewDidLoad
 {
 	[super viewDidLoad];
+	
+	[self fetchCount];
+}
+
+- (void) fetchCount
+{
+	self.highlightsCountButton.hidden = YES;
+	[self.progressSpinner startAnimating];
+
+	NSDictionary* args = @{};
+	
+	RFClient* client = [[RFClient alloc] initWithPath:@"/posts/bookmarks/highlights"];
+	[client getWithQueryArguments:args completion:^(UUHttpResponse* response) {
+		if ([response.parsedResponse isKindOfClass:[NSDictionary class]]) {
+			NSDictionary* info = [response.parsedResponse objectForKey:@"_microblog"];
+			NSInteger num = [[info objectForKey:@"count"] integerValue];
+			
+			RFDispatchMainAsync ((^{
+				if (num == 0) {
+					self.highlightsHeightConstraint.constant = 0;
+				}
+				else {
+					NSString* s;
+					if (num == 1) {
+						s = @"1 highlight";
+					}
+					else {
+						s = [NSString stringWithFormat:@"%ld highlights", (long)num];
+					}
+					
+					[self.highlightsCountButton setTitle:s forState:UIControlStateNormal];
+					self.highlightsCountButton.hidden = NO;
+				}
+				
+				[self.progressSpinner stopAnimating];
+			}));
+		}
+	}];
 }
 
 - (IBAction) showHighlights:(id)sender
